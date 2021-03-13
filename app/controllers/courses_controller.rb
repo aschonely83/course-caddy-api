9b5343c0194b1e8ff548cc093ea0dae1866b2570
aschonely83 <aschonely@gmail.com>
2021-03-13 17:31:23 -0500

    add course

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,38 @@
+class CoursesController < ApplicationController
+  before_action :set_course, only: [:show, :update, :destroy]
+
+  # GET /courses
+  def index
+    @courses = Course.all
+
+    render json: @courses
+  end
+
+  # GET /courses/1
+  def show
+    render json: @course
+  end
+
+  # POST /courses
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      render json: @course, status: :created, location: @course
+    else
+      render json: @course.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_course
+      @course = Course.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def course_params
+      params.require(:course).permit(:name)
+    end
+end
