4fd247d9b083b14194ad54624718124139705ddc
aschonely83 <aschonely@gmail.com>
2021-05-26 19:35:20 -0400

    update model

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,7 +2,7 @@ class Round < ApplicationRecord
   has_one_attached :card
   belongs_to :user
   belongs_to :course
-  validates :par, :score, :card, presence: true
+  validates :par, :score, :card_url, presence: true
 
   def card_url
     Rails.application.routes.url_helpers.url_for(card) if card.attached?
